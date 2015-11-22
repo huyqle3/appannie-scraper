0b75dcbc32493774cde25177e5841218b01be6ea
microwavesam <microwavesam@microwavesam.microwavesam.com>
2015-11-22 13:44:13 -0500

    Small changes to test HTTP Error 503. What headers required?

--- huy/create-iphone-json.py
+++ huy/create-iphone-json.py
@@ -1,13 +1,12 @@
 from bs4 import BeautifulSoup
 import urllib2
 import json
+import argparse
 
-response = urllib2.urlopen("http://huyle.me").read()
+response = urllib2.urlopen("http://www.appannie.com/apps/ios/top/?device=iphone").read()
 print(response)
 
 data = {1: "a", 2: "b", 3: "c"}
 
 with open("iphone-data.json", "w") as output:
 	json.dump(data, output)
-
-print("Hello")
